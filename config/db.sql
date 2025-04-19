397cef5c98de4ae689764fc34b5c180575453419
estebanrp <estebanmilkes@gmail.com>
2025-04-18 22:57:21 -0300

    hola

--- config/db.sql
+++ config/db.sql
@@ -5,6 +5,5 @@ USE todo_list;
 CREATE TABLE IF NOT EXISTS tasks (
   id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  group VARCHAR(255) NOT NULL,
   completed BOOLEAN DEFAULT false
 );
